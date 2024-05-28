50b3d5b1b9700ec82f90bf41c12b8418400fe5ec
Dmitrii Kim <mitya_kim@icloud.com>
2024-05-27 19:45:57 -0700

    Add Chapter 13 Recursion

---
+++ CS_216/Chapter19_Recursion_Functions/lab13_1.cpp
@@ -0,0 +1,4 @@
+// Write a function that accepts an array of integers and a number 
+// indicating the number of elements as arguments. The function should 
+// recursively calculate the sum of all the numbers in the array. 
+// Demonstrate the function in a driver program.
