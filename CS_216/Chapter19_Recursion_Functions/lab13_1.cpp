0ad40f68208c4becf5105a46942223d992d568e2
Dmitrii Kim <mitya_kim@icloud.com>
2024-05-27 20:11:44 -0700

    Add lab1, lab2

--- CS_216/Chapter19_Recursion_Functions/lab13_1.cpp
+++ CS_216/Chapter19_Recursion_Functions/lab13_1.cpp
@@ -2,3 +2,34 @@
 // indicating the number of elements as arguments. The function should 
 // recursively calculate the sum of all the numbers in the array. 
 // Demonstrate the function in a driver program.
+
+#include <iostream>
+    
+using namespace std;
+    
+int sum(int[], const int);
+    
+int main () {
+    const int SIZE = 5;
+    int  intArr[SIZE] = {-1, 2, 6, 3, 5};
+
+    for (int i = 0; i < SIZE; i++) {
+        cout << "Item [" << i + 1 <<"] : " << intArr[i] << endl;
+    }
+
+    cout << "--------------------------------------------" << endl;
+    cout << "         The sum should be : 15" << endl;
+    cout << "--------------------------------------------\n";
+    cout << "The sum using the function : " << sum(intArr, SIZE) << endl;
+    cout << "--------------------------------------------" << endl;
+    
+    return 0;
+}
+
+int sum(int arr[], const int SIZE){
+    if (SIZE > 0) {
+        return (sum(arr, SIZE - 1) + arr[SIZE - 1]);
+    } else {
+        return 0;
+    }
+}
--- CS_216/Chapter19_Recursion_Functions/lab13_2.cpp
+++ CS_216/Chapter19_Recursion_Functions/lab13_2.cpp
@@ -3,3 +3,27 @@
 // argument. For example, if 50 is passed as an argument, the function 
 // will return the sum of 1, 2, 3, 4, … 50. Use recursion to calculate 
 // the sum. Demonstrate the function in a program.
+
+#include<iostream>
+
+using namespace std;
+
+int sum(int n);
+
+int main() {
+    int num;
+
+    cout << "Please enter a number: ";
+    cin >> num;
+    cout << "The sum of " << num << " using recursion is " << sum(num) << endl;
+
+return 0;
+}
+
+int sum(int n) {
+    if (n == 1) {
+        return 1;
+    }
+        
+    return n + sum(n - 1);
+}
